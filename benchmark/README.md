178ea3933604ecd01bb5fc4605cf3807e96ac1b7
IvanMonichev <risen15@yandex.ru>
2025-06-18 22:36:48 +0300

    feat: update test configuration

---
+++ benchmark/README.md
@@ -0,0 +1,7 @@
+# benchmark
+
+## 1
+
+```bash
+k6 run get-orders.test.js --out json=reports/run1.json
+```
--- benchmark/get-orders.test.js
+++ benchmark/get-orders.test.js
@@ -2,19 +2,18 @@ import http from 'k6/http'
 import { check, sleep } from 'k6'
 
 export const options = {
-  vus: 100,             // количество виртуальных пользователей
-  duration: '1m',       // продолжительность теста
+  vus: 100,               // 100 виртуальных пользователей
+  iterations: 10000,      // всего 10 000 запросов (итераций)
   thresholds: {
-    http_req_failed: ['rate<0.01'],       // <1% ошибок
-    http_req_duration: ['p(95)<300'],     // 95% ответов быстрее 300мс
-    http_reqs: ['count>1000']             // минимум 1000 запросов
+    http_req_failed: ['rate<0.01'],         // <1% ошибок
+    http_req_duration: ['p(95)<300'],       // 95% быстрее 300мс
   }
 }
 
 function getRandomUrl() {
   const page = Math.floor(Math.random() * 1000)
   const offset = page * 10
-  return `http://localhost:8020/orders/all?offset=${offset}&limit=10`
+  return `http://localhost:4000/api/orders/all?offset=${offset}&limit=10`
 }
 
 export default function () {
